8f4b8e05a8e6ff8cf25fa60cdaf57c2eef9d5273
ujin-Bang <qkddnwls14@gmail.com>
2021-12-17 19:42:32 +0900

    이름 / 소개 프래그먼트 추가  - 복사 / 붙여넣기 활용

---
+++ app/src/main/java/com/neppplus/viewpager_20211217/fragments/NameFragment.kt
@@ -0,0 +1,23 @@
+package com.neppplus.viewpager_20211217.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.viewpager_20211217.R
+
+class NameFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_name,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
